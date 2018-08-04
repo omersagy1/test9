b821209a91be9b31b7c16bf08c073ca93bb216d9
omersagy <omersagyemails@gmail.com>
2018-08-04 10:31:17 -0400

    call canvas from javascript.

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+console.log('main js starting...');
+console.log(PS.Main.something);
+console.log(PS.Main.x.value0.b);
+
+var c = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
