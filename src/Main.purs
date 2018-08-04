8516f78fa83d5cec0c094ed57863ce5eb438fcc7
omersagy <omersagyemails@gmail.com>
2018-08-04 12:26:22 -0400

    add halogen to project.

--- src/Main.purs
+++ src/Main.purs
@@ -6,7 +6,12 @@ import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Graphics.Canvas (fillRect, getCanvasElementById, getContext2D, setFillStyle)
 import Partial.Unsafe (unsafePartial)
-import Prelude (bind, Unit)
+import Prelude (bind, Unit, unit, discard)
+
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+
+import Button as B
 
 
 main :: Effect Unit
@@ -15,3 +20,9 @@ main = void $ unsafePartial do
   ctx <- getContext2D canvas
   _ <- setFillStyle ctx "#0000FF"
   fillRect ctx { x: 0.0 , y: 0.0 , width: 100.0 , height: 100.0 }
+  hal
+
+hal :: Effect Unit
+hal = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI B.component unit body
