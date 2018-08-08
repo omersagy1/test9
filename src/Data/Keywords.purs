0948fe7a0b3d4aca498c69ea24d62b80a2448915
omersagy <omersagyemails@gmail.com>
2018-08-08 00:57:17 -0400

    move helpers to keywords.purs

---
+++ src/Data/Keywords.purs
@@ -0,0 +1,44 @@
+module Data.Keywords where
+
+import Common.Annex ((|>))
+import Data.Buildable (class Buildable)
+import Game.Types.Condition (Condition(..), PureCondition(..))
+
+import Data.Builder (Builder(..), getConstruct)
+import Game.Types.Story (Story(..))
+import Game.Types.StoryEvent (AtomicEvent(..), StoryEvent(..))
+import Game.Types.TopLevelEvent (body, reoccurring, trigger)
+import Game.Types.TopLevelEvent as TopLevelEvent
+import Prelude (Unit, identity, unit)
+
+
+begin ∷ ∀ s. Builder s Unit → s
+begin = getConstruct
+
+build ∷ ∀ a. Buildable a ⇒ a → Builder a Unit
+build x = BD x unit
+
+ln ∷ String → Builder StoryEvent Unit
+ln text = build (Atomic (Narration text))
+
+restrict ∷ Builder StoryEvent Unit
+restrict = build (Atomic StartInteraction)
+
+never ∷ Condition
+never = Pure Never
+
+reoccuring ∷ Boolean
+reoccuring = true
+
+onlyOnce ∷ Boolean
+onlyOnce = false
+
+top ∷ String → Boolean → Condition → StoryEvent → Builder Story Unit
+top n r c e =
+  let 
+    tl = TopLevelEvent.new n
+               |> (if r then reoccurring else identity)
+               |> (trigger c)
+               |> (body e)
+  in
+    build (Story [tl])
--- src/Data/Scene.purs
+++ src/Data/Scene.purs
@@ -1,47 +1,11 @@
 module Data.Scene where
 
-import Common.Annex ((<|), (|>))
-import Data.Buildable (class Buildable)
-import Game.Types.Condition (Condition(..), PureCondition(..))
+import Prelude (discard)
 
-import Data.Builder (Builder(..), getConstruct)
-import Game.Types.Story (Story(..))
-import Game.Types.StoryEvent (AtomicEvent(..), StoryEvent(..))
-import Game.Types.TopLevelEvent (body, reoccurring, trigger)
-import Game.Types.TopLevelEvent as TopLevelEvent
-import Prelude (Unit, identity, unit, discard)
+import Common.Annex ((<|))
+import Data.Keywords
+import Game.Types.Story (Story)
 
-
-begin ∷ ∀ s. Builder s Unit → s
-begin = getConstruct
-
-build ∷ ∀ a. Buildable a ⇒ a → Builder a Unit
-build x = BD x unit
-
-ln ∷ String → Builder StoryEvent Unit
-ln text = build (Atomic (Narration text))
-
-restrict ∷ Builder StoryEvent Unit
-restrict = build (Atomic StartInteraction)
-
-never ∷ Condition
-never = Pure Never
-
-reoccuring ∷ Boolean
-reoccuring = true
-
-onlyOnce ∷ Boolean
-onlyOnce = false
-
-top ∷ String → Boolean → Condition → StoryEvent → Builder Story Unit
-top n r c e =
-  let 
-    tl = TopLevelEvent.new n
-               |> (if r then reoccurring else identity)
-               |> (trigger c)
-               |> (body e)
-  in
-    build (Story [tl])
 
 story ∷ Story
 story = begin do
