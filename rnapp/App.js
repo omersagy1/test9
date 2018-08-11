e68626229fc32a9d8ab7bc0da2e4de05104c71c4
omersagy <omersagyemails@gmail.com>
2018-08-10 23:05:00 -0400

    remove unnecessary logging

--- rnapp/App.js
+++ rnapp/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import {PS} from './purs_app';
 
 export default class Bananas extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    let s = PS;
-    let s1 = PS.Main.asString;
-    console.log(s1);
+    let story = PS.Main.asString;
     return (
       <View>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Text> {s1} </Text>
+        <Text> {story} </Text>
       </View>
     );
   }
