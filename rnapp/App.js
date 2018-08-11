7ba1bc3717d312dc2ca751e024849aac36a10b95
omersagy <omersagyemails@gmail.com>
2018-08-11 09:18:41 -0400

    main loop implemented.

--- rnapp/App.js
+++ rnapp/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Button, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import {PS} from './purs_app';
 
 const FFI = PS["Main"];
@@ -12,6 +12,14 @@ export default class App extends Component {
     this.state = {
       model: FFI.initialModel
     }
+    requestAnimationFrame(this.loop);
+  }
+
+  loop = (time) => {
+    this.setState(prev => {
+      return { model: FFI.inc(prev.model) };
+    })
+    requestAnimationFrame(this.loop);
   }
 
   increment = () => {
@@ -26,16 +34,45 @@ export default class App extends Component {
     })
   }
 
+  toggle = () => {
+    this.setState(prev => {
+      return { model: FFI.toggle(prev.model) };
+    })
+  }
+
+
   render() {
     const counter = FFI.getCounter(this.state.model);
     return (
-      <View>
-        <Text> Hello, World! </Text>
-        <Text> {counter} </Text>
-        <Button onPress={this.increment} title="Increment Counter" />
-        <Button onPress={this.reset} title="Reset Counter" />
-
+      <View style={styles.app}>
+        <View style={styles.display}>
+          <Text> Hello, World! </Text>
+          <Text> Counter: {counter} </Text>
+        </View>
+        <View style={styles.buttonSection}>
+          <Button onPress={this.increment} title="Increment Counter" />
+          <Button onPress={this.reset} title="Reset Counter" />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  app: {
+    margin: 10,
+    flex: 1,
+    justifyContent: 'center'
+  },
+
+  display: {
+    backgroundColor: "lightblue",
+    flex: 0,
+    alignItems: "center"
+  },
+
+  buttonSection: {
+    backgroundColor: "lightgreen"
+  }
+
+});
